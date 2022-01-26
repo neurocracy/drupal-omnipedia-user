e31fcc3e7c8ef2ccf28d92730a0ae00e145d1501
Matei Stanca <i@ambientimpact.com>
2022-01-26 15:00:41 -0500

    Added omnipedia.user_roles service:
    Currently provides a basic userHasElevatedRole() method to check if a
    provided user account is considered as having a role with elevated
    permissions.

---
+++ src/Service/UserRoles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\omnipedia_user\Service;
+
+use Drupal\omnipedia_user\Service\UserRolesInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * The Omnipedia user roles service interface.
+ */
+class UserRoles implements UserRolesInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @todo Find a more thorough heuristic for whether a user has what can be
+   *   considered an elevated role or permissions. Alternatively, add a
+   *   configuration form that allows selecting which roles are to be considered
+   *   as having elevated roles.
+   */
+  public function userHasElevatedRole(AccountInterface $user): bool {
+
+    // The assumption here is that if a user can access administration pages,
+    // they very likely have some sort of elevated permission. Note that this is
+    // a really crude heuristic that doesn't cover instances where an account
+    // has some sort of elevated permission but cannot access admin pages.
+    return $user->hasPermission('access administration pages');
+
+  }
+
+}
---
+++ src/Service/UserRolesInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\omnipedia_user\Service;
+
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * The Omnipedia user roles service interface.
+ */
+interface UserRolesInterface {
+
+  /**
+   * Determine if the provided user has a role with elevated permissions.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $user
+   *   The user account to check.
+   *
+   * @return boolean
+   *   True if the user has an elevated role, false if they don't.
+   */
+  public function userHasElevatedRole(AccountInterface $user): bool;
+
+}
