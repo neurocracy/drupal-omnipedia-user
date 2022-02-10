7f36692f88b3b59760b4fee28228af7aa24cc149
Matei Stanca <i@ambientimpact.com>
2022-02-10 12:30:58 -0500

    UserElevatedRolesEventSubscriber: abstracted repetitive stuff:
    Added log method that automatically populates the 'link' and '%name'
    context variables.
    
    Now requires PHP 8.

--- src/EventSubscriber/Omnipedia/UserElevatedRolesEventSubscriber.php
+++ src/EventSubscriber/Omnipedia/UserElevatedRolesEventSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\omnipedia_user\EventSubscriber\Omnipedia;
 
+use Drupal\Core\Logger\RfcLogLevel;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesBlockedEvent;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesChangedEvent;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesCreatedEvent;
@@ -11,6 +12,7 @@ use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesEventInterface;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesGrantedEvent;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesLoggedInEvent;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesUnblockedEvent;
+use Drupal\user\UserInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -55,6 +57,57 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Log a message to the logger.
+   *
+   * @param string $message
+   *   The untranslated message to log.
+   *
+   * @param array $context
+   *   Context variables to pass to the logger, i.e. to replace placeholders in
+   *   $message.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   The user entity to log a message about.
+   *
+   * @param string|int $severity
+   *   The severity level to log. Defaults to
+   *   \Drupal\Core\Logger\RfcLogLevel::NOTICE
+   */
+  protected function log(
+    string $message, array $context = [],
+    UserInterface $user,
+    string|int $severity = RfcLogLevel::NOTICE
+  ): void {
+
+    // Add a link to the user if not set.
+    if (!isset($context['link'])) {
+      $context['link'] = $this->getUserLink($user);
+    }
+
+    // Provide %name if not set.
+    if (!isset($context['%name'])) {
+      $context['%name'] = $user->getDisplayName();
+    }
+
+    $this->loggerChannel->log($severity, $message, $context);
+
+  }
+
+  /**
+   * Get a rendered link for the provided user account.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   The user entity to return a link for.
+   *
+   * @return string
+   *   The rendered link, suitable for the 'link' context for a log message.
+   */
+  protected function getUserLink(UserInterface $user): string {
+    return $user->toLink('View user', 'canonical', ['absolute' => true])
+      ->toString();
+  }
+
+  /**
    * Perform actions when a user with elevated roles has been blocked.
    *
    * @param \Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesBlockedEvent $event
@@ -62,12 +115,8 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onBlocked(UserElevatedRolesBlockedEvent $event): void {
 
-    $this->loggerChannel->notice(
-      'A user with elevated roles was blocked: %name',
-      [
-        'link'  => $event->getUser()->toLink('View user')->toString(),
-        '%name' => $event->getUser()->getDisplayName(),
-      ]
+    $this->log(
+      'A user with elevated roles was blocked: %name', [], $event->getUser()
     );
 
   }
@@ -79,6 +128,9 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    *   The event object.
    */
   public function onChanged(UserElevatedRolesChangedEvent $event): void {
+
+    /** @var \Drupal\user\UserInterface */
+    $targetUser = $event->getUser();
 
     /** @var string */
     $rolesAdded   = \implode(', ', $event->getAddedRoles());
@@ -99,14 +151,13 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
       $rolesRemoved = 'none';
     }
 
-    $this->loggerChannel->notice(
+    $this->log(
       'A user has had elevated roles changed: %name <br>Roles added: %rolesAdded <br>Roles removed: %rolesRemoved',
       [
-        'link'          => $event->getUser()->toLink('View user')->toString(),
-        '%name'         => $event->getUser()->getDisplayName(),
         '%rolesAdded'   => $rolesAdded,
         '%rolesRemoved' => $rolesRemoved,
-      ]
+      ],
+      $targetUser
     );
 
   }
@@ -119,12 +170,8 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onCreated(UserElevatedRolesCreatedEvent $event): void {
 
-    $this->loggerChannel->notice(
-      'A user with elevated roles was created: %name',
-      [
-        'link'  => $event->getUser()->toLink('View user')->toString(),
-        '%name' => $event->getUser()->getDisplayName(),
-      ]
+    $this->log(
+      'A user with elevated roles was created: %name', [], $event->getUser()
     );
 
   }
@@ -137,11 +184,8 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onDeleted(UserElevatedRolesDeletedEvent $event): void {
 
-    $this->loggerChannel->notice(
-      'A user with elevated roles was deleted: %name',
-      [
-        '%name' => $event->getUser()->getDisplayName(),
-      ]
+    $this->log(
+      'A user with elevated roles was deleted: %name', [], $event->getUser()
     );
 
   }
@@ -154,13 +198,12 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onDenied(UserElevatedRolesDeniedEvent $event): void {
 
-    $this->loggerChannel->notice(
+    $this->log(
       'A user has had all elevated roles removed: %name <br>Roles removed: %roles',
       [
-        'link'    => $event->getUser()->toLink('View user')->toString(),
-        '%name'   => $event->getUser()->getDisplayName(),
         '%roles'  => \implode(', ', $event->getRemovedRoles()),
-      ]
+      ],
+      $event->getUser()
     );
 
   }
@@ -173,13 +216,12 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onGranted(UserElevatedRolesGrantedEvent $event): void {
 
-    $this->loggerChannel->notice(
+    $this->log(
       'A user has had one or more elevated roles granted: %name <br>Roles added: %roles',
       [
-        'link'    => $event->getUser()->toLink('View user')->toString(),
-        '%name'   => $event->getUser()->getDisplayName(),
         '%roles'  => \implode(', ', $event->getAddedRoles()),
-      ]
+      ],
+      $event->getUser()
     );
 
   }
@@ -192,12 +234,8 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onLoggedIn(UserElevatedRolesLoggedInEvent $event): void {
 
-    $this->loggerChannel->notice(
-      'A user with elevated roles has logged in: %name',
-      [
-        'link'  => $event->getUser()->toLink('View user')->toString(),
-        '%name' => $event->getUser()->getDisplayName(),
-      ]
+    $this->log(
+      'A user with elevated roles has logged in: %name', [], $event->getUser()
     );
 
   }
@@ -210,12 +248,8 @@ class UserElevatedRolesEventSubscriber implements EventSubscriberInterface {
    */
   public function onUnblocked(UserElevatedRolesUnblockedEvent $event): void {
 
-    $this->loggerChannel->notice(
-      'A user with elevated roles was unblocked: %name',
-      [
-        'link'  => $event->getUser()->toLink('View user')->toString(),
-        '%name' => $event->getUser()->getDisplayName(),
-      ]
+    $this->log(
+      'A user with elevated roles was unblocked: %name', [], $event->getUser()
     );
 
   }
