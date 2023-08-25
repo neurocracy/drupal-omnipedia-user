e86055587065ed7b57d189c9e1b8b1a0de3f1f43
Matei Stanca <i@ambientimpact.com>
2023-08-25 08:47:52 -0400

    Use with elevated roles log in Rules action now extends our base event:
    This brings it in line with the other events and was likely a mistake.

--- src/Plugin/RulesAction/UserElevatedRolesLoggedInDispatch.php
+++ src/Plugin/RulesAction/UserElevatedRolesLoggedInDispatch.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\omnipedia_user\Plugin\RulesAction;
 
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesEventInterface;
 use Drupal\omnipedia_user\Event\Omnipedia\UserElevatedRolesLoggedInEvent;
-use Drupal\rules\Core\RulesActionBase;
+use Drupal\omnipedia_user\Plugin\RulesAction\AbstractUserElevatedRolesDispatch;
 use Drupal\user\UserInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Provides a 'Dispatch event when a user with elevated role(s) has logged in' action.
@@ -27,46 +24,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
  *   }
  * )
  */
-class UserElevatedRolesLoggedInDispatch extends RulesActionBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The Symfony event dispatcher service.
-   *
-   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
-   */
-  protected EventDispatcherInterface $eventDispatcher;
-
-  /**
-   * {@inheritdoc}
-   *
-   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
-   *   The Symfony event dispatcher service.
-   */
-  public function __construct(
-    array $configuration, $pluginId, $pluginDefinition,
-    EventDispatcherInterface $eventDispatcher
-  ) {
-
-    parent::__construct($configuration, $pluginId, $pluginDefinition);
-
-    $this->eventDispatcher = $eventDispatcher;
-
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(
-    ContainerInterface $container,
-    array $configuration, $pluginId, $pluginDefinition
-  ) {
-    return new static(
-      $configuration,
-      $pluginId,
-      $pluginDefinition,
-      $container->get('event_dispatcher')
-    );
-  }
+class UserElevatedRolesLoggedInDispatch extends AbstractUserElevatedRolesDispatch {
 
   /**
    * Dispatch an event when this action is executed.
